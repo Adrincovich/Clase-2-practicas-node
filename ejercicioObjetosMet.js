8f3e6598744b33ca19a69b702f5ee04c6def56fe
Adrincovich <adrian.drincovich@gmail.com>
2022-08-27 04:41:05 -0300

    Resuelvo ejercicio 2

--- ejercicioObjetosMet.js
+++ ejercicioObjetosMet.js
@@ -14,7 +14,7 @@ const filterAlumnos = alumnos
     .filter(item => item.nota > 6)
     .map(item => item.numAlumno);
 
-    // console.log(filterAlumnos);
+    // console.log(alumnos);
 
 // al armar un array nuevo completamente distinto, se usa map
 
@@ -26,7 +26,7 @@ const filterAlumnos = alumnos
 // Dado el siguiente array de productos en stock, agregar a cada producto un atributo booleano agotado
 // en true si el stock es mayor a 0, caso contrario false.
 
-let productos = [
+const productos = [
     { id: "1", nombre: "lapiceras", stock: 164 },
     { id: "2", nombre: "marcadores", stock: 0 },
     { id: "3", nombre: "cartulinas", stock: 25 },
@@ -34,6 +34,21 @@ let productos = [
     { id: "5", nombre: "mochilas", stock: 4 }
 ]
 
+
+function chequeo() {
+    productos.forEach((a) =>{
+        if (a.stock == 0) {
+            return a.agotado = true
+        }if (a.stock > 0 ){
+            return a.agotado = false
+          }
+        })
+}
+
+productos.map(item => item.agotado = "")
+         .map(chequeo)
+
+console.log(productos)
 
 
 // -----------°-----------°-----------°-----------°-----------°-----------°-----------°-----------°
